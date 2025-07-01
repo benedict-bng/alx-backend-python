2793c3a7fd608da895bc31020349c6a5ecbf8d46
Benedict Nderitu <giteebenedict@gmail.com>
2025-07-01 10:21:13 +0300

    Create 2-lazy_paginate.py

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,21 @@
+# 2-lazy_paginate.py
+
+import seed  # your existing seed.py with DB connection funcs
+
+def paginate_users(page_size, offset):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
+
+def lazy_pagination(page_size):
+    offset = 0
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
